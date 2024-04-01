4dd8a5841d0c0be6f573b0e0d4f73945ba36af4e
HaerilRapli <146050867+HaerilRapli@users.noreply.github.com>
2024-04-01 12:34:32 +0800

    1 april 2024

---
+++ STRUKTURDATA/meet2.py
@@ -0,0 +1,20 @@
+# belajar variabel
+# assigment
+x = 3
+y = 6
+uang = 1000
+
+# pemanggilan
+print ("Nilai Uas saya =", x)
+print ("Nilai Uas saya =", y)
+print ("Nilai Uas saya =", uang)
+
+# penamaan variabel
+nilai_a = 9
+kotak20 = 10
+
+# input variabel kedua
+x = 30
+print ("Nilai Uas saya =", x)
+x = 300
+print ("Nilai Uas saya =", x)
