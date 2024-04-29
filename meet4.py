2dccc0df32d6e62f139571aec58c7e4c59439d62
HaerilRapli <146050867+HaerilRapli@users.noreply.github.com>
2024-04-29 11:51:37 +0800

    aritmatika, 29 april 2024

---
+++ meet4.py
@@ -0,0 +1,49 @@
+# inputan dari user
+# aritmatika
+
+# belajar inputan
+# haeril = input("masukan kata : ")
+# print("isi dari haeril :", haeril, "bertipe data :", type(haeril))
+
+# belajar aritmatika dasar
+x = 9
+y = 4
+
+# contoh penjumlahan
+hasil = x + y
+print("x + y =", hasil)
+
+# pengurangan
+hasil = x - y
+print("x - y =", hasil)
+
+# perkalian
+hasil = x * y
+print("x * y =", hasil)
+
+# pembagian
+hasil = x / y
+print("x / y =", hasil)
+
+# perpangkatan exponen **
+hasil = x ** y
+print("x ^ y =", hasil)
+
+# modus %
+hasil = y % x
+print("x % y =", hasil)
+
+# floor divission (pembulatan kebawah) //
+hasil = x // y
+print("x // y", hasil)
+
+# aritmatika prioritas
+# (), exponen, perkalian/pembagian, perjumlahan/pengurangan
+x = 3
+y = 4
+z = 5
+
+hasil = ( x ** y * (z + x) / y - z % x // y)
+print(hasil)
+
+
