c5457be539a8bfbfb5d294c8aa8d71f41206f5bf
HaerilRapli <146050867+HaerilRapli@users.noreply.github.com>
2024-06-03 11:47:23 +0800

    Add files via upload

---
+++ meet9.py
@@ -0,0 +1,27 @@
+# percabangan jika maka
+
+from xml.dom.minidom import NamedNodeMap
+
+
+name = input("what is your name")
+# if kondisi : aksi
+
+
+# if standar
+# if name == "haeril" : print("mahasiswa")
+# print("end of program")
+
+# if indentasi
+#if name == "haeril" :
+#    print("mahasiswa")
+#    print("males")
+# print("end of program")
+
+# 3. IF ELSE
+if name=="haeril":
+    print("ML")
+    print("terimakasih {name}")
+else:
+    print("FF")
+print("end of program")
+
